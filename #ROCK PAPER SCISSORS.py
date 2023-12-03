ae5792a29bad9abf8862e78fc651ee52aa415f59
Panashe Julius <131316602+PjPikes@users.noreply.github.com>
2023-12-03 08:39:06 +0200

    Add files via upload

---
+++ #ROCK PAPER SCISSORS.py
@@ -0,0 +1,51 @@
+#ROCK PAPER SCISSORS
+import random
+
+weapon = ["rock", "paper", "scissors"] #list of weapons for computer to choose from
+
+computer = random.choice(weapon) #this function will randomise what the computer will pick from weapons list
+
+print("!!! welcome to this game of rock sham bow !!!!" )
+
+player_one = input("choose your weapon!!! rock, paper or scissors \U0001F600 ---") #the input function will get user input 
+
+
+
+if player_one == "rock" and computer == "scissors":
+    print(f"computer plays {computer}")
+    print("player one wins!")
+       
+elif player_one == "scissors" and computer == "paper":
+    print(f"computer plays {computer}")
+    print("player one wins!")
+     
+elif player_one == "paper" and computer == "rock":
+    print(f"computer plays {computer}")
+    print("player one wins!")
+
+elif computer == "rock" and  player_one == "scissors":
+    print(f"computer plays {computer}")
+    print("computer wins!")
+    
+elif computer == "scissors" and player_one == "paper":
+    print(f"computer plays {computer}")
+    print("computer wins!")
+        
+elif computer == "paper" and player_one == "rock":
+    print(f"computer plays {computer}")
+    print("computer wins!")
+        
+elif player_one == computer:
+    print(f"computer plays {computer}")
+    print("its a draw")
+
+
+
+
+
+
+
+
+#player1
+#can pick between rock paper  scissors 
+#use if statement to compare between p1 and computer
